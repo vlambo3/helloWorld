201f2f907c2fdb62960a7c72b1dff67f4c35c23f
vlambo3 <contadora.godoy.vanina@gmail.com>
2024-03-27 12:28:47 -0300

    add test

--- helloWorld.py
+++ helloWorld.py
@@ -1,3 +1,10 @@
 for i in range(10) :
     print("Hello World " + str(i))
-exit(0)
+
+i = 5 + 5
+assert (i == 10)
+
+# the exit code tell us if the program work
+# 0 means it works, anything else means it did not work
+# exit(0)
+
