75a815e9fdf5ad3761bfe0c52968e0f589a7bcd9
vlambo3 <contadora.godoy.vanina@gmail.com>
2024-03-27 12:30:05 -0300

    add an error

--- helloWorld.py
+++ helloWorld.py
@@ -2,7 +2,7 @@ for i in range(10) :
     print("Hello World " + str(i))
 
 i = 5 + 5
-assert (i == 10)
+assert (i != 10)
 
 # the exit code tell us if the program work
 # 0 means it works, anything else means it did not work
